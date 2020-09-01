429bcd1f80cb2993561dbbf6e0d4cd6cf1ac040a
by <1132989278@qq.com>
2020-09-01 21:11:01 +0800

    修改my.cnf 配置

--- my.cnf.sh
+++ my.cnf.sh
@@ -6,10 +6,11 @@ character_set_server=utf8
 collation-server=utf8_general_ci
 
 [client]
-character_set_client=utf8
+
 
 [mysql]
 default_character_set=utf8
 " >> /etc/my.cnf
 
 cat /etc/my.cnf
+service mysqld restart
