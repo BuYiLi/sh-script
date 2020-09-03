91d67f6422eb4eb688f8f1e1a4c84675cb7327d9
by <1132989278@qq.com>
2020-09-03 10:26:00 +0800

    Ubuntu增加： 修改DNS脚本

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -4,8 +4,15 @@ set -e
 configSSH(){
     configFile=/etc/ssh/sshd_config
     sed -i "s|^#PermitRootLogin prohibit-password$|PermitRootLogin yes|" $configFile
+    sed -i "s|^#Port 22$|Port 22|" $configFile
     service sshd restart
 }
 
+modifyDNS(){
+    configFile=/etc/systemd/resolved.conf
+    sed -i "s|^#DNS=$|DNS=223.5.5.5 223.6.6.6|" $configFile
+}
+
 configSSH
+modifyDNS
 
