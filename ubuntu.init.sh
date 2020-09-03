5c573de61068a36acd9c5b62af3bdef81c84760e
by <1132989278@qq.com>
2020-09-03 19:46:39 +0800

    修改ubuntu软件源

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+modifySourcesList(){
+    sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak
+
+    sudo sed -i "s@http://.*archive.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
+    sudo sed -i "s@http://.*security.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
+    apt-get update
+}
+
 configSSH(){
     configFile=/etc/ssh/sshd_config
     sed -i "s|^#PermitRootLogin prohibit-password$|PermitRootLogin yes|" $configFile
@@ -14,6 +22,7 @@ modifyDNS(){
     sed -i "s|^#DNS=$|DNS=223.5.5.5 223.6.6.6|" $configFile
 }
 
+modifySourcesList
 configSSH
 modifyDNS
 
